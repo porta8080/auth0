630c6c9d9c5de67af1a5a048b012bd170058a4c1
porta8080 <adm.porta8080@gmail.com>
2016-12-21 09:12:52 -0200

    passport auth0

---
+++ index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var Auth0Strategy = require('passport-auth0'),
+    passport = require('passport');
+
+var app = express();
+
+var strategy = new Auth0Strategy({
+   domain:       'your-domain.auth0.com',
+   clientID:     'your-client-id',
+   clientSecret: 'your-client-secret',
+   callbackURL:  '/callback'
+  },
+  function(accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+
+app.get('/callback',
+  passport.authenticate('auth0', { failureRedirect: '/login' }),
+  function(req, res) {
+    if (!req.user) {
+      throw new Error('user null');
+    }
+    res.redirect("/");
+  }
+);
+
+app.get('/login',
+  passport.authenticate('auth0', {}), function (req, res) {
+  res.redirect("/");
+});
